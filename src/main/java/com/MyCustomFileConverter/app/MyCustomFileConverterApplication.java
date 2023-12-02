1a68dbafc9f49ec1ff350b16e7ae7dd5e5918027
saurabhkawatra <saurabh.kawatra1@gmail.com>
2023-12-02 20:57:51 +0530

    first commit

---
+++ src/main/java/com/MyCustomFileConverter/app/MyCustomFileConverterApplication.java
@@ -0,0 +1,13 @@
+package com.MyCustomFileConverter.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCustomFileConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyCustomFileConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MyCustomFileConverter/app/MyCustomFileConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyCustomFileConverter.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCustomFileConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
